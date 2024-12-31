8945a4039c3af94dcd639eadafcde07ef53e19de
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-31 09:38:55 +0600

    51-3 Set auth provider and auth context

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
 
-  const handleRegister = e =>{
-       e.preventDefault()
-  }
+    const handleRegister = e => {
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        
+        console.log(name, email, password)
+    }
 
     return (
         <div className="hero bg-base-200 ">
@@ -18,6 +23,12 @@ const Register = () => {
                     <form onSubmit={handleRegister} className="card-body">
                         <div className="form-control">
                             <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="email" name='email' placeholder="email" className="input input-bordered" required />
@@ -27,12 +38,10 @@ const Register = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                           
                         </div>
                         <div className="form-control mt-6">
-                            <button  className="btn btn-primary">Register</button>
+                            <button className="btn btn-primary">Register</button>
                         </div>
                     </form>
                     <p className='ml-4 mb-4'>
